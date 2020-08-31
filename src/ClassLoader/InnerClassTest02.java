d9faa322be48318c5870efd82859f00ac82cd10a
axikuazei <692978358@qq.com>
2020-08-31 22:28:34 +0800

    init gitignore

--- src/ClassLoader/InnerClassTest02.java
+++ src/ClassLoader/InnerClassTest02.java
@@ -1,8 +1,13 @@
 package ClassLoader;
 
 public class InnerClassTest02 {
-    public static void main(String[] args){
-        
+    public static void main(final String[] args) {
+        InnerClassTest innerClassTest = new InnerClassTest();
+        //在外部实例化：
+        //普通内部类：需要依托外部类实例
+        innerClassTest.new InnerA();
+        //静态内部类：不需要外部类实例，通过 new Outclass.InnerClass()
+        new InnerClassTest.InnerC();
     }
     
 }
