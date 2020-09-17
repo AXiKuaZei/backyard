d1c236fa110c5320e40143ac4cbaac5691b38377
axikuazei <692978358@qq.com>
2020-09-17 13:19:24 +0800

    x

--- src/concurrent/ThreadPool01.java
+++ src/concurrent/ThreadPool01.java
@@ -1,5 +1,27 @@
 package concurrent;
 
 public class ThreadPool01 {
-    
+
+    static int y =2;
+
+    public static void main(String[] args){
+        int x = 1;
+
+        Runnable r1 = new Runnable(){
+            int z = 3;
+            @Override
+            public void run(){
+                System.out.println(x);
+                System.out.println(y);
+                System.out.println(z++);
+            }
+
+        };
+        Thread a = new Thread(r1);
+        Thread b = new Thread(r1);
+        a.start();
+        b.start();
+
+        
+    }
 }
